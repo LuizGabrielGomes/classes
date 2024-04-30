123f7a312f37fcd4da00124a4438e1a0ad1ce562
Luiz Gabriel Gomes <luizgabrielgomes9686@gmail.com>
2024-04-30 18:44:34 +0100

    exercicio javascript

---
+++ function/deverFuction.html/functiondever/function.js
@@ -0,0 +1,39 @@
+
+
+    function myFunction(text) {
+        return text.length 
+    }
+
+
+    myFunction("Artificial Intelligence plural artificial intelligences uncountable noun. Artificial intelligence is a type of computer technology which is concerned with making machines work in an intelligent way, similar to the way that the human mind works. The abbreviation AI is also used");
+
+    console.log( myFunction("Artificial Intelligence plural artificial intelligences uncountable noun. Artificial intelligence is a type of computer technology which is concerned with making machines work in an intelligent way, similar to the way that the human mind works. The abbreviation AI is also used"))
+
+    
+   
+
+ let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ let length = text.length;
+
+ console.log(length)
+
+
+
+
+ 
+function contarArray(animals) {  
+    return animals.length
+}
+
+const retorno = contarArray(["urso" , "gato" , "cachorro" , "peixe" , "pato", "cavalo"])
+     
+console.log(retorno) 
+
+
+// <script>
+// let x = myFunction(4, 3);
+// document.getElementById("demo").innerHTML = x;
+
+// function myFunction(a, b) {
+//   return a * b;
+// }
